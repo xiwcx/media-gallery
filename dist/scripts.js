ab2ed0ff74ec54fbecd217b269a7ae8afd9bef58
Welch Canavan <welch.canavan@gmail.com>
2015-08-10 00:58:33 -0400

    angular templating

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,3 +1,41 @@
 'use strict';
 
 console.log('hello, world');
+
+var gallery = angular.module('gallery', []);
+
+gallery.controller('Gallery', function ($scope) {
+  $scope.galleryItems = [{
+    date: "2015-08-09",
+    img: "http://www.fillmurray.com/g/1000/1000",
+    title: "Bill Murray"
+  }, {
+    date: "2015-08-08",
+    img: "http://www.placecage.com/1000/1000",
+    title: "Nic Cage"
+  }, {
+    date: "2015-08-07",
+    img: "http://www.stevensegallery.com/1000/1000",
+    title: "Steven Segal"
+  }, {
+    date: "2015-08-06",
+    img: "http://www.nicenicejpg.com/1000/1000",
+    title: "Robert Van Winkle"
+  }, {
+    date: "2015-08-05",
+    img: "http://www.placebear.com/1000/1000",
+    title: "A Bear"
+  }, {
+    date: "2015-08-04",
+    img: "http://placekitten.com/1000/1000",
+    title: "A Kitten"
+  }, {
+    date: "2015-08-09",
+    img: "http://www.fillmurray.com/g/1000/1000",
+    title: "Bill Murray"
+  }, {
+    date: "2015-08-08",
+    img: "http://www.placecage.com/1000/1000",
+    title: "Nic Cage"
+  }];
+});
